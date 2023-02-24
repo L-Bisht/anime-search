99b82f0eeb248fd090d9e3ca9d353be9d0b6fe73
Lalit Bisht <lbisht1996@gmail.com>
2023-02-24 12:40:31 +0530

    2nd commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,31 +3,7 @@ import axios from "axios";
 import "./App.css";
 
 function App() {
-  useEffect(() => {
-    const encodedParams = new URLSearchParams();
-    encodedParams.append("accessToken", "<REQUIRED>");
-    encodedParams.append("userId", "<REQUIRED>");
-
-    const options = {
-      method: "POST",
-      url: "https://anilistmikilior1v1.p.rapidapi.com/getAnimeList",
-      headers: {
-        "content-type": "application/x-www-form-urlencoded",
-        "X-RapidAPI-Key": "fc37b89ba9mshdd6569e704f56bbp18777djsna1800dbe7285",
-        "X-RapidAPI-Host": "Anilistmikilior1V1.p.rapidapi.com",
-      },
-      data: encodedParams,
-    };
-
-    axios
-      .request(options)
-      .then(function (response) {
-        console.log(response.data);
-      })
-      .catch(function (error) {
-        console.error(error);
-      });
-  });
+  useEffect(() => {});
 
   return <div className="App">test</div>;
 }
