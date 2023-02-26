291c2be80a689833ac860201ea7769140021daa0
Lalit Bisht <lbisht1996@gmail.com>
2023-02-26 20:55:41 +0530

    centered text

--- src/components/CharacterCard/CharacterCard.tsx
+++ src/components/CharacterCard/CharacterCard.tsx
@@ -8,21 +8,18 @@ function CharacterCard({ character }: Tprops) {
   return (
     <div
       key={character.mal_id}
-      className="w-30 h-50 rounded-lg shadow-md hover:cursor-pointer lg:max-w-sm m-2 border-2 border-gray-400 px-4 py-6 rounded-lg transform transition duration-500 hover:scale-105"
+      className="w-25 rounded-lg shadow-md hover:cursor-pointer lg:max-w-sm m-2 border-2 border-gray-400 px-4 py-6 rounded-lg transform transition duration-500 hover:scale-105"
     >
       <img
         src={character.images?.jpg?.image_url || ""}
         alt={character.name}
         className="ml-auto mr-auto h-48"
       />
-      <div className="p-4">
-        <div className="content-center">
-          <h4 className="text-xl font-bold tracking-tight text-black-600">
-            {character.name?.slice(0, 10) +
-              (character.name.length > 10 ? "..." : "")}
-          </h4>
-        </div>
-        <p className="mb-2 leading-normal">
+      <div className="p-2">
+        <h4 className="text-xl text-center font-bold tracking-tight text-black-600">
+          {character.name}
+        </h4>
+        <p className="mb-2 text-center leading-normal">
           {character.about?.slice(0, 50) + "..."}
         </p>
         {/* <a
